868185cdf9508656d36d3d015d099558f23eb23a
ruqayaalsammak <ruqayaalsammak2017@gmail.com>
2026-08-27 16:11:32 +0300

    complete the create  inside Task  Controller

---
+++ controllers/tasks.js
@@ -0,0 +1,16 @@
+const Task = require('../models/Task.js')
+
+const create = async (req, res) => {
+    try {
+        req.body.eventId = req.params.eventId
+        const task = await Task.create(req.body)
+        res.status(201).json(task)
+    } catch (err) {
+        res.status(500).json({ err: err.message })
+    }
+}
+
+
+module.exports = {
+    create,
+}
