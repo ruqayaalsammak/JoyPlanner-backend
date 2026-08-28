39c9da2dd300e1bdd604efe97cfe60c2f5b4ce0d
ruqayaalsammak <ruqayaalsammak2017@gmail.com>
2026-08-28 14:31:16 +0300

    update  the index   inside user  Controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,12 @@
-const User = require('../models/user')
+const User = require('../models/user.js')
 
 const index = async (req, res) => {
-   const users = await User.find()
-   res.json(users)
+    try {
+        const users = await User.find({})
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json({ err: err.message })
+    }
 }
 
 module.exports = {
